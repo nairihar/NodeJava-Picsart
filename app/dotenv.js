c6f29521bfe471d3fd01f52d4adbd7354dfa9b7f
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2021-02-16 21:04:59 +0400

    Add dotenv project

---
+++ app/dotenv.js
@@ -0,0 +1,9 @@
+// read .env file
+// process each line
+// fill values into process.env
+
+process.env.EMAIL = 'email';
+
+// let GIT_EMAIL = ""
+// let GIT_EMAIL = '""'
+
---
+++ app/index.js
@@ -0,0 +1,21 @@
+require('./dotenv').config();
+// require('dotenv').config();
+
+
+console.log(process.env.GIT_EMAIL)
+console.log(process.env.GIT_PASS)
+
+
+
+/// require module.export
+
+`
+sad
+as
+dsa
+d
+dsa
+s
+ad
+s
+dsas`
